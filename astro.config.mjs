17ef0a40179592b86ad68c0a2a563639c8d388d2
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-02-28 17:01:59 +0100

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,101 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Fluid",
+      social: {
+        github: "https://github.com/thomasKn",
+      },
+      customCss: ["./src/tailwind.css"],
+      sidebar: [
+        {
+          label: "Getting started",
+          items: [
+            {
+              label: "Introduction",
+              link: "/getting-started/introduction",
+            },
+            {
+              label: "Why Fluid?",
+              link: "/getting-started/why-fluid",
+            },
+            {
+              label: "Installation",
+              link: "/getting-started/installation",
+            },
+          ],
+        },
+        {
+          label: "Using the CMS",
+          items: [
+            {
+              label: "Content structure",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Using templates",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Color schemes",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Typography",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Theme content",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Settings",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Preview mode",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Internationalization",
+              link: "/cms/preview-mode/",
+            },
+            {
+              label: "Deployment",
+              link: "/cms/deployment/",
+            },
+          ],
+        },
+        {
+          label: "Theme development",
+          items: [
+            {
+              label: "Project structure",
+              link: "/theme/introduction",
+            },
+            {
+              label: "Sections",
+              link: "/theme/introduction",
+            },
+            {
+              label: "Components",
+              link: "/theme/why-fluid",
+            },
+            {
+              label: "Groqd queries",
+              link: "/theme/installation",
+            },
+          ],
+        },
+      ],
+    }),
+    react(),
+    tailwind(),
+  ],
 });
---
+++ src/content/docs/guides/deployment.md
@@ -0,0 +1,9 @@
+---
+title: Deployment
+---
+
+## Deploy to Oxygen
+
+## Deploy to Vercel
+
+## Set up a preview domain
